f4b6fe19d5afaf4c862a8a42c2ddc067929dc4f3
ricardfiguls <ricard.figuls@gmail.com>
2018-02-11 23:23:03 +0100

    adding functionallity (more options)

--- module.js
+++ module.js
@@ -14,15 +14,16 @@ const LIST = 1
  * creates tree object with content
  */
 const TREE = 2
-// native fs module
-const fs = require('fs')
+// native modules
+const FS = require('fs')
+const PATH = require('path')
 /**
  * returns a Promise with Stats info of the file
  * @param {string} file 
  */
 async function stat(file) {
     return new Promise(function (resolve, reject) {
-        fs.stat(file, function (err, stats) {
+        FS.stat(file, function (err, stats) {
             if (err) {
                 // console.error(err)
                 reject(err);
@@ -36,13 +37,13 @@ async function stat(file) {
  * Returns a Promise with an objects info array
  * @param {string} path the path to be searched for
  */
-async function myReaddir(path) {
+async function myReaddir(path, settings, deep) {
     const data = []
     return new Promise(function (resolve, reject) {
 
         try {
             // Asynchronously computes the canonical pathname by resolving ., .. and symbolic links.
-            fs.realpath(path, function (err, rpath) {
+            FS.realpath(path, function (err, rpath) {
                 if (err)
                     rpath = path
 
@@ -50,7 +51,7 @@ async function myReaddir(path) {
                 rpath = normalizePath(rpath)
 
                 // Reading contents of path
-                fs.readdir(rpath, function (err, files) {
+                FS.readdir(rpath, function (err, files) {
 
                     // If error reject them
                     if (err) {
@@ -65,7 +66,10 @@ async function myReaddir(path) {
                                 'path': rpath,
                                 'fullname': rpath + '/' + files[i]
                             }
-
+                            if (settings.extensions)
+                                obj.extension = (PATH.extname(files[i])).toLowerCase()
+                            if (settings.deep)
+                                obj.deep = deep
                             data.push(obj);
                         }
 
@@ -94,10 +98,12 @@ function normalizePath(path) {
  * @param {*} settings 
  * @param {*} progress 
  */
-async function listDir(path, settings, progress) {
+async function listDir(path, settings, progress, deep) {
     let list
+    if (deep == undefined)
+        deep = 0;
     try {
-        list = await myReaddir(path);
+        list = await myReaddir(path, settings, deep);
     } catch (err) {
         return { 'error': err, 'path': path }
     }
@@ -111,9 +117,9 @@ async function listDir(path, settings, progress) {
                     list[i].stats = stats
                 if (list[i].isDirectory && settings.recursive) {
                     if (settings.mode == LIST)
-                        list = list.concat(await listDir(list[i].fullname, settings, progress))
+                        list = list.concat(await listDir(list[i].fullname, settings, progress, deep+1))
                     else {
-                        list[i].content = await listDir(list[i].fullname, settings, progress)
+                        list[i].content = await listDir(list[i].fullname, settings, progress, deep+1)
                         if (list[i].content.length == 0)
                             list[i].content = null
                     }
@@ -137,10 +143,10 @@ async function listDir(path, settings, progress) {
  * @param {object} options options (mode, recursive, stats, ignoreFolders)
  * @param {function} progress callback with item data and progress info for each item
  */
-async function dir(path, options, progress) {
+async function list(path, options, progress) {
 
     // options skipped?
-    if (typeof options == 'function') 
+    if (typeof options == 'function')
         progress = options
 
     // Setting default settings
@@ -148,7 +154,9 @@ async function dir(path, options, progress) {
         mode: LIST,
         recursive: true,
         stats: false,
-        ignoreFolders: true
+        ignoreFolders: true,
+        extensions: false,
+        deep: false
     }
 
     // Aplying options (if set)
@@ -161,14 +169,20 @@ async function dir(path, options, progress) {
             settings.stats = options.stats
         if (options.ignoreFolders != undefined)
             settings.ignoreFolders = options.ignoreFolders
+        if (options.deep != undefined)
+            settings.deep = options.deep
+        if (options.extensions != undefined)
+            settings.extensions = options.extensions
     }
 
     // Reading contents
     return await listDir(path, settings, progress);
 }
 module.exports = {
-    list: dir,
-    stat: stat,
     LIST: LIST,
-    TREE: TREE
+    TREE: TREE,
+    list: list,
+    stat: stat,
+    fs: FS,
+    path: PATH
 }
