8a4e042cf8a305d8270097a5a7dbe60d1b654963
Ricard Figuls <ricard.figuls@gmail.com>
2019-03-12 22:27:13 +0100

    UPDATE travis ci files for push after_success

---
+++ .travis/push.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+git_push() {
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git remote add gh https://${GITHUB_REPO_TOKEN}@github.com/m0rtadelo/recursive-readdir-async.git > /dev/null 2>&1
+  git push origin gh
+}
+setup_git
+git_push
