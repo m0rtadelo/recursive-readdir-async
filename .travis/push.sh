c7eb321742484182ad6f63fce79c94913551cb70
Ricard Figuls <ricard.figuls@gmail.com>
2019-03-12 22:43:48 +0100

    FIX push

--- .travis/push.sh
+++ .travis/push.sh
@@ -9,7 +9,7 @@ git_push() {
   git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
   git remote add gh https://${GITHUB_REPO_TOKEN}@github.com/m0rtadelo/recursive-readdir-async.git > /dev/null 2>&1
-  git push origin gh
+  git push gh
 }
 setup_git
 git_push
